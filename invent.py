9b79d01979380ed2809f81898d501b78bb222cf8
Victor van WIeringen <Victorvwier@hotmail.com>
2021-11-10 13:21:04 +0100

    Adapt invent to use tokens instead of strings

--- invent.py
+++ invent.py
@@ -1,18 +1,19 @@
 import itertools
 from common_environment.abstract_tokens import Token
 from common_environment.control_tokens import If
+from string_environment.string_tokens import *
 
-
-# Takes a list of tokens and returns a list of all permutations where len(tokenSet) > len(per) > 1 
+# Takes a list of tokens and returns a list of all permutations where len(tokenSet) > len(per) > 1
 def invent(tokenSet, maxLength) -> list:
     if (maxLength <= 1):
         return []
     return list(itertools.permutations(tokenSet, maxLength)) + invent(tokenSet, maxLength - 1)
 
+
 def invent2(tokenSet, boolTokenSet, maxLength) -> list:
     # Normal invention step
     out = invent(tokenSet, maxLength)
-    
+
     # Generating if statements
     if maxLength > 3:
         if_list = []
@@ -26,11 +27,16 @@ def invent2(tokenSet, boolTokenSet, maxLength) -> list:
         out = out + if_list
     return out
 
+
 if __name__ == "__main__":
+
+    # Test for robot environment
+    bool_tokens = {AtStart,	AtEnd, IsLetter, IsNotLetter, IsUppercase,	IsNotUppercase,
+                   IsLowercase,	IsNotLowercase,	IsNumber,	IsNotNumber,	IsSpace,	IsNotSpace}
     iftoken = "IF"
-    bool_tokens = {"TRUE", "FALSE", "MAYBE"}
-    normal_tokens = {"UP", "DOWN", "LEFT", "RIGHT", "PICKUP", "DROP"}
+    #bool_tokens = {"TRUE", "FALSE", "MAYBE"}
+    normal_tokens = {MoveRight, MoveLeft, Drop, MakeLowercase, MakeUppercase}
+
+    #normal_tokens = {"UP", "DOWN", "LEFT", "RIGHT", "PICKUP", "DROP"}
     for t in invent2(normal_tokens, bool_tokens, 7):
-        print(type(invent2(normal_tokens, bool_tokens, 8)))
-        break
-    
+        print(t)
