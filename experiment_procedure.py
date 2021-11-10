f7fbb16bc6c47f17184e68239293317aa61b7ef0
Nadia <nadia.matulewicz@gmail.com>
2021-11-10 15:19:37 +0100

    start with abstracting domains from examples

--- experiment_procedure.py
+++ experiment_procedure.py
@@ -8,36 +8,55 @@ import time
 
 # TODO check if i still call the correct invent method (invent vs invent2)
 from invent import invent2
-from typing import List
+from typing import List, Tuple
+from common_environment.environment import *
+from interpreter.interpreter import *
 
 MAX_TOKEN_FUNCTION_DEPTH = 3
 MAX_NUMBER_OF_ITERATIONS = 120
 
 
-class Example
+class Example:
+    def __init__(self, input_environment: Environment, output_environment: Environment):
+        self.input_environment = input_environment
+        self.output_environment = output_environment
 
 class TestCase:
-    def __init__(self, training_examples, test_examples, distance_function, tokens, boolean_tokens):
+    def __init__(self, training_examples: List[Example], test_examples: List[Example], tokens, boolean_tokens):
         self.training_examples = training_examples # tuple consisting of input environment and wanted output environment
         self.test_examples = test_examples  # tuple consisting of input environment and wanted output environment
-        self.tokens = tokens
-        self.boolean_tokens = boolean_tokens
 
 
 class Experiment:
-    def __init__(self, name: str, test_cases: List[TestCase]):
+    def __init__(self, name: str, domain_name: str, test_cases: List[TestCase]):
         self.name = name
+        self.domain_name = domain_name
         self.test_cases = test_cases
+
+
+def extract_domain_from_environment(environment):
+    domain_name = "unknown"
+    if isinstance(environment, RobotEnvironment): 
+        domain_name = "robot"
+    elif isinstance(environment, StringEnvironment): 
+        domain_name = "string"
+    elif isinstance(environment, PixelEnvironment): 
+        domain_name = "pixel"
+    return domain_name
+
 
 
 # a single case exists of several examples which should be solved by one single program
 def test_performance_single_case(test_case: TestCase):
-    # generate different token combinations
     start_time = time.time()
+    # generate different token combinations
+
     token_functions = invent2(test_case.tokens, test_case.boolean_tokens, MAX_TOKEN_FUNCTION_DEPTH)
     # find program that satisfies training_examples
-    program = search(token_functions, test_case.training_examples, MAX_NUMBER_OF_ITERATIONS)
+    program: Program
+    program, best_loss, solved  = search(token_functions, test_case.training_examples, MAX_NUMBER_OF_ITERATIONS)
     finish_time = time.time()
+
     execution_time_in_seconds = start_time - finish_time
     successes = 0
     for (in_state, out_state) in test_case.test_examples:
@@ -73,7 +92,7 @@ def write_performances_of_experiments_to_file(experiments: List[Experiment], out
     lines_to_write = []
     for experiment in experiments:
         average_success_percentage, average_execution_time, percentage_of_completely_successful_programs = \
-            test_performance_single_experiment(experiment.test_cases)
+            test_performance_single_experiment(experiment.test_cases, experiment.domain_name)
         lines_to_write.append("Experiment name: " + experiment.name + "\n")
         lines_to_write.append("Average_success_percentage: " + str(average_success_percentage) + "\n")
         lines_to_write.append("Average_execution_time: " + str(average_execution_time) + "\n")
@@ -86,12 +105,17 @@ def write_performances_of_experiments_to_file(experiments: List[Experiment], out
     file.close()
 
 
-def get_experiments():
-    # TODO get experiments from correct files
-    experiment = ()
+def get_all_experiments():
+    # TODO get experiments from correct files using get_single_experiments
+    return experiments
+    
+
+def get_single_experiment(): 
+    # TODO get single experiment
+    return experiment
 
 if __name__ == "__main__":
     write_performances_of_experiments_to_file(
-        get_experiments(),
+        get_all_experiments(),
         "performance_results/results.txt"
     )
