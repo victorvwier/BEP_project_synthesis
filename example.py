7ce97c743333d06d198cbf6fe6bb0d7261fda759
Stef <rasing.stef@gmail.com>
2021-11-15 11:25:05 +0100

    added example of testing groups of files

--- example.py
+++ example.py
@@ -1,10 +1,21 @@
-from experiment_procedure import test_performance_single_case_and_write_to_file
+from experiment_procedure import *
+from parser.pixel_parser import PixelParser
 from parser.robot_parser import RobotParser
 from parser.string_parser import StringParser
 from robot_environment import robot_tokens
 from string_environment import string_tokens
 
 if __name__ == "__main__":
-    testcase = StringParser().parse_file("1-1-1.pl")
-    (succesper, time) = test_performance_single_case_and_write_to_file(testcase, string_tokens.TransTokens, string_tokens.BoolTokens)
+    testcase = PixelParser().parse_file("0-0-1.pl")
+    (succesper, time) = test_performance_single_case_and_write_to_file(testcase, pixel_tokens.TransTokens, pixel_tokens.BoolTokens)
     print("Test case worked for {}% of the time, running time: {}".format(succesper, time))
+
+    """
+    Use this to run groups of test case defined by file prefix
+    
+    experiment = StringParser().parse_all(file_prefix="1-2-")
+    (ave_suc, ave_time, com_suc) = test_performance_single_experiment(experiment)
+    print("Experiment had an average success rate of {}%, average running time: {}, and {}% of programs were "
+          "completely successful".format(ave_suc, ave_time, com_suc))
+        
+    """
