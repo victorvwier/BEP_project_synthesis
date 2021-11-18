2637f60207cb0ff21ae9e758cbdb73026a2a963d
dahkraf <farhadfahrer.a@gmail.com>
2021-11-18 20:37:23 +0000

    Combined loss calculation with program evaluation in brute (no zipping)

--- search/brute/brute.py
+++ search/brute/brute.py
@@ -20,72 +20,46 @@ class Brute(SearchAlgorithm):
         # find program that satisfies training_examples
         program: Program
         return _search(token_functions, test_case.training_examples, MAX_NUMBER_OF_ITERATIONS)
-        
 
-def print_p(p):
-    print(p.sequence)
-
-def print_ps(ps):
-    l = []
-    for p in ps:
-        l.append(p.sequence)
-    print(l)
-
-def loss(output_pairs):
-    return sum([p[0].distance(p[1]) for p in output_pairs])
-
-def problem_solved(output_pairs):
-    return all(map(lambda p : p[0].correct(p[1]), output_pairs))
 
 # takes 90 % of our time
-def evaluate_program(program, sample_inputs, sample_outputs):
-    program_outputs = []
+def evaluate_program(program, examples):
     try:
-        for input in sample_inputs:
+        cum_loss = 0.0
+        solved = True
+        for example in examples:
+            input = example.input_environment
+            output = example.output_environment
             program_output = program.interp(input)
-            program_outputs.append(program_output)
-        output_pairs = list(zip(program_outputs, sample_outputs))
-        cum_loss = loss(output_pairs)
-        solved = problem_solved(output_pairs)
+            cum_loss += program_output.distance(output)
+            solved = solved and program_output.correct(output)
         if (solved):
             return (cum_loss, 0, program)
         return ( cum_loss, 1, program)
     except (InvalidTransition, RecursiveCallLimitReached, LoopIterationLimitReached) as e:
         return (float("inf"), 1, program)
 
-def extend_program(best_program, programs, tokens: List[Token], sample_inputs, sample_outputs):
+def extend_program(best_program, programs, tokens: List[Token], examples):
     for token in tokens:
         potentially_better_program = Program(best_program.sequence + [copy.copy(token)])
-        program_new = evaluate_program(potentially_better_program, sample_inputs, sample_outputs)
+        program_new = evaluate_program(potentially_better_program, examples)
         if program_new[0] != float('inf'):
             heapq.heappush(programs, program_new)
-    #updated_programs = sorted(updated_programs, key=lambda x: (x[2], x[1]))
     return programs
 
-# def prioritize_programs(programs, sample_inputs, sample_outputs):
-#     ordered_programs = []
-#     for program in programs:
-#         ordered_programs.append(evaluate_program(program, sample_inputs, sample_outputs))
-#     ordered_programs = sorted(ordered_programs, key=lambda x: (x[2], x[1]))
-#     return ordered_programs
 
-# def find_best_program(programs, sample_inputs, sample_outputs):
-#     return prioritize_programs(programs, sample_inputs, sample_outputs)[0]
-
-def synth_loop(programs, tokens: List[Token], sample_inputs, sample_outputs, iteration, num_iterations):
+def synth_loop(programs, tokens: List[Token], examples, iteration, num_iterations):
     (best_loss, solved, best_program) = heapq.heappop(programs)
 
     if(iteration >= num_iterations or solved == 0):
         return best_loss, solved, best_program
 
-    updated_programs = extend_program(best_program, programs, tokens, sample_inputs, sample_outputs)
+    updated_programs = extend_program(best_program, programs, tokens, examples)
 
     iteration += 1
-    return synth_loop(updated_programs, tokens, sample_inputs, sample_outputs, iteration, num_iterations)
+    return synth_loop(updated_programs, tokens, examples, iteration, num_iterations)
 
 def _search(tokens: List[Token], examples: List[Example], num_iterations):
-    sample_inputs = [e.input_environment for e in examples]
-    sample_outputs = [e.output_environment for e in examples]
     #domain_tokens = [Program([t]) for t in tokens]
     
     #program_dictionary = copy.deepcopy(initial_programs)
@@ -94,6 +68,6 @@ def _search(tokens: List[Token], examples: List[Example], num_iterations):
     
     starting_heap = [(float('inf'), 1, program)]
     heapq.heapify(starting_heap)
-    best_loss, solved, best_program = synth_loop(starting_heap, tokens, sample_inputs, sample_outputs, 0, num_iterations)
+    best_loss, solved, best_program = synth_loop(starting_heap, tokens, examples, 0, num_iterations)
 
     return best_program, best_loss, solved
--- test_brute_search.py
+++ test_brute_search.py
@@ -1,32 +1,25 @@
-from brute_search import *
+from search.brute.brute import *
+
+def print_programs(programs):
+    for program in programs:
+        print(program)
 
 def test_extend_program():
     P1 = Program([MoveDown(), MoveRight()])
     P2 = Program([MoveUp(), MoveLeft()])
     P3 = Program([MoveDown(), MoveUp()])
-    updated_programs = extend_program(P1, [P1, P2, P3])
-    print_ps(updated_programs)
+    updated_programs = extend_program(P1, [P2, P3], [MoveDown(), MoveRight(), MoveUp(), MoveLeft()])
+    print_programs(updated_programs)
 
-def test_find_best_program():
-    P1 = Program([MoveDown(), Draw()])
-    P2 = Program([MoveRight(), MoveLeft()])
-    P3 = Program([MoveDown(), MoveRight(), Draw()])
-    start_state = PixelEnvironment(2, 0, 0, [[False, False], [False, False]])
-    end_state = PixelEnvironment(2, 1, 1, [[False, False], [False, True]])
-    best_program, _, solved = find_best_program([P1, P2, P3], [start_state], [end_state])
-    print_p(best_program)
-    print(solved)
-
-def test_search():
+def test__search():
     tokens = [MoveDown(), MoveRight(), Draw()]
     start_state = PixelEnvironment(2, 2, 0, 0, [[False, False], [False, False]])
     end_state = PixelEnvironment(2, 2, 1, 1, [[False, False], [False, True]])
     num_iterations = 3
-    best_program, _, solved = search(tokens, [(start_state, end_state)], num_iterations)
-    print_p(best_program)
+    best_program, _, solved = _search(tokens, [Example(start_state, end_state)], num_iterations) # Why is _search not defined
+    print(best_program)
     print(solved)
 
 
 # test_extend_program()
-# test_find_best_program()
-# test_search()
+# test__search()
