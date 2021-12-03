443ff175045e284abf079b265ec56b9ee8951855
Stef <rasing.stef@gmail.com>
2021-12-03 11:34:48 +0100

    setup two lns implementations for parameter tuning

--- search/vlns/large_neighborhood_search/algorithms/multi_method_lns.py
+++ search/vlns/large_neighborhood_search/algorithms/multi_method_lns.py
@@ -1,4 +1,6 @@
-from evaluation.experiment_procedure import extract_trans_tokens_from_domain_name, extract_bool_tokens_from_domain_name
+from evaluation.experiment_procedure import extract_trans_tokens_from_domain_name, extract_bool_tokens_from_domain_name, \
+    StringEnvironment
+from example_parser.robot_parser import RobotParser
 from example_parser.string_parser import StringParser
 from search.vlns.large_neighborhood_search.accept.deterministic_accept import DeterministicAccept
 from search.vlns.large_neighborhood_search.accept.stochastic_accept import StochasticAccept
@@ -11,50 +13,62 @@ from search.vlns.large_neighborhood_search.repair.multi_method_repair import Mul
 from search.vlns.large_neighborhood_search.repair.random_repair import RandomRepair
 from search.vlns.large_neighborhood_search.repair.stochastic_sequence_repair import StochasticSequenceRepair
 from search.vlns.large_neighborhood_search.repair.stochastic_single_repair import StochasticSingleRepair
+from search.vlns.parameter_tuning import ParameterTuning
 
 
 class MultiMethodLNS(LNS):
 
-    def __init__(self):
+    def __init__(self, par):
         super().__init__(
             time_limit=1,
-            accept=StochasticAccept(initial_temperature=100, cooling_factor=0.95),
+            accept=StochasticAccept(initial_temperature=100, cooling_factor=0.97),
             #accept=DeterministicAccept(),
             destroy=MultiMethodDestroy(
                 methods=[
-                    SingleDestroy(p_env=0.15, p_bool=0.0),
+                    SingleDestroy(p_env=0.2, p_bool=0.3),
                     SequenceDestroy(p_destroy=0.5, max_seq_size=5),
                     BlockDestroy(p_destroy=0.5)
                 ],
-                weights=[1, 1, 1],
+                weights=[1,1,1],
             ),
             repair=MultiMethodRepair(
                 methods=[
-                    RandomRepair(p_if=0.1, p_loop=0.2, p_remove=0, p_split=0.5),
+                    RandomRepair(w_if=1, w_loop=1, w_remove=1, w_split=1, w_default=1),
                     StochasticSingleRepair(
-                        p_if=0.1, p_loop=0.3, p_remove=0.1, p_split=0.3,
+                        p_if=0.2, p_loop=0.2, p_remove=0.2, p_split=0.2,
                         n_if=10, n_loop=10, n_bool=20, n_env=20),
                     StochasticSequenceRepair(
-                        p_if=0.1, p_loop=0.2, p_remove=0, p_split=0,
-                        n_seq=100, n_bool=20, max_sequence_size=10)
+                        p_if=0.3, p_loop=0.3, p_remove=0, p_split=0,
+                        n_seq=20, n_bool=20, max_sequence_size=10)
                 ],
-                weights=[1, 0, 3]
+                weights=[1, 1, 1]
             ),
         )
 
 if __name__ == "__main__":
-    mmlns = MultiMethodLNS()
+    pt = ParameterTuning(
+        domain="string",
+        files=([1,2,3,4,5,6,7,8,9],range(102, 103),range(1,6)),
+        repetitions=10,
+        search_cons=MultiMethodLNS,
+        values=[1],
+    )
 
-    tc = StringParser().parse_file("9-2-1.pl")
+    pt.run()
+
+if __name__ == "__main__0":
+    mmlns = MultiMethodLNS(0)
+
+    tc = StringParser().parse_file("9-103-1.pl")
     envs = set([c() for c in extract_trans_tokens_from_domain_name("string")])
     bools = set([c() for c in extract_bool_tokens_from_domain_name("string")])
 
-    r = mmlns.run(tc, envs, bools)
+    r= mmlns.run(tc.training_examples, envs, bools)
     p = r.dictionary["program"]
-    c = mmlns.cost_train(tc, p)
+    c = mmlns.cost(tc.test_examples, p)
 
     print("Final program: {}".format(p))
     print("Cost: {}".format(c))
     print("Execution time: {}".format(r.dictionary["execution_time"]))
-    print(r.dictionary['best_cost_per_iteration'])
-    print(r.dictionary['current_cost_per_iteration'])
+    #print(r.dictionary['best_cost_per_iteration'])
+    #print(r.dictionary['current_cost_per_iteration'])
--- search/vlns/large_neighborhood_search/algorithms/single_destroy_random_repair.py
+++ search/vlns/large_neighborhood_search/algorithms/single_destroy_random_repair.py
@@ -13,17 +13,18 @@ class SingleDestroyRandomRepair(LNS):
             time_limit=1,
             #accept=StochasticAccept(initial_temperature=100, cooling_factor=0.95),
             accept=DeterministicAccept(),
-            destroy=SingleDestroy(p_env=0.3, p_bool=0.2),
-            repair=RandomRepair(w_loop=1.2, w_if=1.4, w_remove=par, w_split=1.1, w_default=1),
+            destroy=SingleDestroy(p_env=0.15, p_bool=0.2),
+            repair=RandomRepair(w_loop=1, w_if=1, w_remove=1, w_split=par, w_default=1),
             #repair=StochasticSingleRepair(p_if=0.1, p_loop=0.3, p_remove=0.1, p_split=0.3, n_if=10, n_loop=10, n_bool=20, n_env=20),
         )
 
 if __name__ == "__main__":
     pt = ParameterTuning(
         domain="string",
-        files=([1,3,5,7,9],range(51,56),range(1,5)),
+        files=([9],[56, 61, 71, 80, 87, 92, 96, 101, 102, 103, 114, 116, 121, 127, 137],range(1,5)),
+        repetitions=1,
         search_cons=SingleDestroyRandomRepair,
-        values=[0.1, 0.4, 0.7, 1.0, 1.3, 1.6, 1.9, 2.2],
+        values=[0.4, 0.6, 0.8, 1.0, 1.2, 1.4, 1.6, 1.8],
     )
 
     pt.run()
