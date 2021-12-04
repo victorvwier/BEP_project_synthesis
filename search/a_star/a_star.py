45d98c773f3ff640342a712c66a4832bc9577b30
basjenneboer <bjenneboer@gmail.com>
2021-12-04 20:41:27 +0100

    create custom priority queue that supports replacing priorities

--- search/a_star/a_star.py
+++ search/a_star/a_star.py
@@ -1,7 +1,8 @@
 import itertools
 import json
+from dataclasses import field
 from heapq import *
-from typing import Callable, Generator, Iterator
+from typing import Callable, Generator, Iterator, Union
 
 from common.environment import Environment
 from common.experiment import Example
@@ -16,29 +17,82 @@ MAX_NUMBER_OF_ITERATIONS = 20
 MAX_TOKEN_FUNCTION_DEPTH = 3
 
 
-class AStar(SearchAlgorithm):
-    # _best_program: Program
-    # input_envs: tuple[Environment]
-    # output_envs: tuple[Environment]
-    # tokens: list[Token]
-    # loss_function: Callable[[int, int], int]
-    # program_generator: Iterator[Program]
+@dataclass(init=True, order=True)
+class Node:
+    priority: float  # TODO: make sure these are always int
+    count: int
+    item: any = field(compare=False)
 
+
+class UniquePriorityQueue:
+
+    def __init__(self):
+        self.heap: list[Node] = list()
+        self.item_set: dict[any, Union[Node, None]] = dict()
+        self.count = itertools.count()
+
+    def insert(self, item: any, priority: float) -> bool:
+        """
+        Inserts item with given priority in queue or update the priority if the item is already present.
+        @param item: Object to insert in the queue
+        @param priority: Items with a lower priority will be popped first
+        @return: True if item was already present and updated, False otherwise
+        """
+        updated = False
+        if item in self.item_set:
+            self.item_set[item].item = None
+            updated = True
+        new_node = Node(priority, next(self.count), item)
+        heappush(self.heap, new_node)
+        self.item_set[item] = new_node
+        return updated
+
+    def pop(self) -> Union[tuple[any, float], None]:
+        """
+        Removes and returns item with lowest priority.
+        If the lowest priority is shared by two items, returns last added item
+        @return: Tuple consisting of item with lowest priority and the value of priority
+        """
+        while self.heap:
+            node = heappop(self.heap)
+            if node.item is not None:
+                self.item_set.pop(node.item)
+                return node.item, node.priority
+        raise IndexError("Queue is empty.")
+
+    def __bool__(self):
+        """
+        Returns whether the queue has items
+        @return: True if queue contains one or more items, False otherwise.
+        """
+        while self.heap and self.heap[0].item is None:
+            heappop(self.heap)
+        return bool(self.heap)
+
+
+class AStar(SearchAlgorithm):
 
     def setup(self, training_examples: List[Example], trans_tokens: set[Token], bool_tokens: set[Token]):
         self.input_envs: tuple[Environment] = tuple(e.input_environment for e in training_examples)
         self.output_envs: tuple[Environment] = tuple(e.output_environment for e in training_examples)
         self.tokens: list[Token] = invent2(trans_tokens, bool_tokens, MAX_TOKEN_FUNCTION_DEPTH)
         self.loss_function: Callable[[int, int], int] = lambda g, h: g + h
-        self.program_generator: Iterator[Program] = self.best_first_search(self.input_envs, self.output_envs, self.tokens, self.loss_function, self._heuristic_min)
-        self._best_program = Program([])
+        self.program_generator: Iterator[Union[Program, None]] = self.best_first_search_upq(
+            self.input_envs, self.output_envs, self.tokens, self.loss_function, self._heuristic_min)
         self._expanded_programs: list[Program] = list()
 
     def iteration(self, training_example: List[Example], trans_tokens: set[Token], bool_tokens: set[Token]) -> bool:
-        if p := next(self.program_generator):
-            self._best_program = p
+        try:
+            if p := next(self.program_generator):
+                # a solution was found: record solution and stop iterating
+                self._best_program = p
+                return False
+            # no solution found yet: continue iterating
+            return True
+        except StopIteration:
+            # nothing left to explore: stop iterating (will never happen)
             return False
-        return True
+
 
     def extend_result(self, search_result: SearchResult):
         search_result.dictionary['expanded_programs'] = self._expanded_programs
@@ -50,7 +104,7 @@ class AStar(SearchAlgorithm):
 
     @staticmethod
     def _heuristic(from_states: tuple[Environment], to_states: tuple[Environment]) -> float:
-        return sum(map(lambda tup: tup[0].distance(tup[1]), zip(from_states, to_states)))/len(from_states)
+        return sum(map(lambda tup: tup[0].distance(tup[1]), zip(from_states, to_states))) / len(from_states)
 
     @staticmethod
     def _heuristic_min(from_states: tuple[Environment], to_states: tuple[Environment]) -> float:
@@ -65,28 +119,39 @@ class AStar(SearchAlgorithm):
         sequence.reverse()
         return Program(sequence)
 
-    def best_first_search(self, start_node: tuple[Environment], end_node: tuple[Environment], tokens: list[Token], f, h) -> Iterator[Program]:
+    def best_first_search_upq(self, start_node: tuple[Environment], end_node: tuple[Environment],
+                              tokens: list[Token], f, h) -> Iterator[Program]:
+        if self._correct(start_node, end_node):
+            yield Program([])
         reached = {start_node: (0, False, False)}  # for each reached node: (path_cost, previous_node, token_used)
-        queue = []
-        count = itertools.count()
-        heappush(queue, (f(0, h(start_node, end_node)), next(count), start_node))
-        node = False
+        queue = UniquePriorityQueue()
+        gcost = 0
+        hcost = h(start_node, end_node)
+        fcost = f(gcost, hcost)
+        queue.insert(start_node, fcost)
+        node = None
         while queue:
-            total_cost, _, node = heappop(queue)  # total_cost: the estimated cost for the total path
-            path_cost, _, _ = reached[node]  # path_cost: the minimal cost so far for a path to this node
-            self._expanded_programs.append(self._find_program(node, reached))
-            if self._correct(node, end_node):
-                break
-            else:
-                yield False
+            node, fcost = queue.pop()
+            gcost, _, _ = reached[node]
+            # self._expanded_programs.append(self._find_program(node, reached))
+            solution = None
             node_copies = [copy.deepcopy(node) for _ in tokens]
             for token, node_copy in zip(tokens, node_copies):
                 try:
                     child = tuple(map(token.apply, node_copy))
-                    if child not in reached or path_cost + token.number_of_tokens(1) < reached[child][0]:
-                        reached[child] = path_cost + token.number_of_tokens(1), node, token
-                        heappush(queue, (f(path_cost + token.number_of_tokens(1), h(child, end_node)), next(count), child))
-                except(InvalidTransition, RecursiveCallLimitReached, LoopIterationLimitReached) as e:
+                    if solution is None and self._correct(child, end_node):
+                        solution = child  # check early if we've found a solution
+                    gcost_child = gcost + token.number_of_tokens()
+                    # if child was not yet expanded or our new gcost is the smallest up until now
+                    if child not in reached or gcost_child < reached[child][0]:
+                        reached[child] = gcost_child, node, token
+                        hcost_child = h(child, end_node)
+                        fcost_child = f(gcost_child, hcost_child)
+                        queue.insert(child, fcost_child)
+                except(InvalidTransition, RecursiveCallLimitReached, LoopIterationLimitReached):
                     pass
-        program = self._find_program(node, reached)
-        yield program
+            if solution:
+                yield self._find_program(solution, reached)
+            else:
+                yield None
+        return
