96bf39ef6503d396b795462f1a8641dc474b2fce
Stef <rasing.stef@gmail.com>
2021-12-13 10:26:13 +0100

    LNS class

--- search/vlns/large_neighborhood_search/large_neighborhood_search.py
+++ search/vlns/large_neighborhood_search/large_neighborhood_search.py
@@ -1,109 +1,155 @@
-import copy
 import time
-from statistics import mean
+from collections import Callable
 
-from common.experiment import TestCase, Example
+from common.experiment import Example
 from common.prorgam import Program
-from common.tokens.abstract_tokens import Token, EnvToken, BoolToken, InvalidTransition
-from common.tokens.control_tokens import LoopIterationLimitReached
-
+from common.tokens.abstract_tokens import EnvToken, BoolToken, TransToken
 from search.abstract_search import SearchAlgorithm
 from search.search_result import SearchResult
 from search.vlns.large_neighborhood_search.accept.accept import Accept
 from search.vlns.large_neighborhood_search.destroy.destroy import Destroy
+from search.vlns.large_neighborhood_search.invent.variable_depth_invent import VariableDepthInvent
 from search.vlns.large_neighborhood_search.repair.repair import Repair
-from search.vlns.large_neighborhood_search.tokens.sequence_token import SeqToken
-
 
 class LNS(SearchAlgorithm):
+    """Implements the abstract Large Neighborhood Search algorithm given an Accept, Destroy and Repair method. Also a
+    time limit can be set."""
 
-    def __init__(self, time_limit: float, accept: Accept, destroy: Destroy, repair: Repair):
+    def __init__(self, time_limit: float, accept: Accept, destroy: Destroy, repair: Repair,
+                 invent: Callable[[list[TransToken], list[BoolToken]], VariableDepthInvent], increase_depth_after: Callable[[int], int], debug: bool = False):
         super().__init__(time_limit)
         self.accept = accept
         self.destroy = destroy
         self.repair = repair
+        self.invent = invent
+        self.increase_depth_after = increase_depth_after
+        self.debug = debug
 
-        self.c_best = float('inf')
-        self.x_current = None
-        self.c_current = float('inf')
-        self.iteration_number = 1
+        self.sol_current = None
+        self.cost_best = -1
+        self.cost_current = -1
 
-        self.best_cost_per_iteration = []
-        self.current_cost_per_iteration = []
+        self.iterations_since_last_best = 0
 
-        self.time_destroy = 0
-        self.time_repair = 0
-        self.time_cost = 0
+        self.stats = {}
 
-    def setup(self, test_case: list[Example], trans_tokens: set[EnvToken], bool_tokens: set[BoolToken]):
-        self.repair.set_token_libraries(trans_tokens, bool_tokens)
-        self.repair.cost = lambda s: self.cost_seq(test_case, s)
+    def setup(self, test_case: list[Example], trans_tokens: list[TransToken], bool_tokens: list[BoolToken]):
+        self.repair.invent = self.invent(trans_tokens, bool_tokens)
+        self.accept.reset()
 
-        self.c_best = self.cost(test_case, self.best_program)
-        self.x_current = Program([])
-        self.c_current = self.c_best
-        self.iteration_number = 1
+        self._best_program = Program([])
+        self.sol_current = Program([])
+        self.cost_best = self.cost(exs=test_case, p=self._best_program)
+        self.cost_current = self.cost_best
 
-        self.best_cost_per_iteration = []
-        self.current_cost_per_iteration = []
+        self.cost_dict = {}
+        self.iterations_since_last_best = 0
 
-        self.time_destroy = 0
-        self.time_repair = 0
-        self.time_cost = 0
+        self.stats['iterations'] = 0
+        self.stats['search_depth'] = 1
+        self.stats['average_visited_length'] = 0
+        self.stats["time_destroy"] = 0
+        self.stats["time_repair"] = 0
+        self.stats["time_cost"] = 0
+        self.stats["best_cost_per_iteration"] = []
+        self.stats["current_cost_per_iteration"] = []
+        self.stats["explored_per_depth"] = {k: 0 for k in range(1, self.stats["search_depth"]+1)}
+        self.stats["multiple_explored_per_depth"] = {k: 0 for k in range(1, self.stats["search_depth"]+1)}
 
-    def iteration(self, test_case: list[Example], trans_tokens: set[EnvToken], bool_tokens: set[BoolToken]) -> bool:
-        #print("Program ({}): {}".format(self.c_current, self.x_current))
+    def iteration(self, test_case: list[Example], tokens: list[EnvToken], bt) -> bool:
+        self.debug_print("\n")
+        self.debug_print("-=-=-=[Iteration {}]=-=-=-".format(self.stats['iterations']))
+        self.debug_print("Program ({}): {}".format(self.cost_current, self.sol_current))
         t_b = time.process_time()
 
-        destroyed = self.destroy.destroy(self.x_current)
+        # Destroy current solution
+        destroyed = self.destroy.destroy(self.sol_current)
         t_d = time.process_time()
-        self.time_destroy += (t_d - t_b)
-        #print("Destroyed: {}".format(destroyed))
+        self.debug_print("Destroyed: {}".format(destroyed))
 
+        # Repair destroyed solution into temporary solution
         x_temp = self.repair.repair(destroyed)
         t_r = time.process_time()
-        self.time_repair += (t_r - t_d)
 
-        c_temp = self.cost(test_case, x_temp)
+        # Calculate cost of temporary solution
+        #c_temp = self.cost(test_case, x_temp)
+        c_temp = self.eff_cost(test_case, x_temp)
         t_c = time.process_time()
-        self.time_cost += (t_c - t_r)
-        #print("Repaired ({}): {}".format(c_temp, x_temp))
-
-        self.best_cost_per_iteration.append(self.c_best.__round__(1))
-        self.current_cost_per_iteration.append(self.c_current.__round__(1))
+        self.debug_print("Repaired ({}): {}".format(c_temp, x_temp))
 
         # New best solution found
-        if c_temp < self.c_best:
-            self.c_best = c_temp
-            self._best_program = copy.deepcopy(x_temp)
+        if c_temp < self.cost_best:
+            self.cost_best = c_temp
+            self._best_program = x_temp
+
+            self.iterations_since_last_best = 0
+
+            pair = (self.stats["iterations"], self.cost_best.__round__(2))
+            self.stats["best_cost_per_iteration"].append(pair)
+        else:
+            self.iterations_since_last_best += 1
+
+            if self.iterations_since_last_best >= self.increase_depth_after(self.stats["search_depth"]):
+                d = self.stats["search_depth"] + 1
+
+                self.stats["search_depth"] = d
+                self.stats["explored_per_depth"][d] = 0
+                self.stats["multiple_explored_per_depth"][d] = 0
+
+                self.destroy.increment_search_depth()
+                self.repair.increment_search_depth()
+                self.repair.invent.increment_depth()
+
+                #self.accept.reset()
+                #self.sol_current = Program([])
+                #self.cost_current = self.cost(test_case, self.sol_current)
+
+                self.iterations_since_last_best = 0
 
         # Accepted as new current solution
-        if self.accept.accept(self.c_current, c_temp, self.x_current, x_temp, self.iteration_number):
-            self.x_current = x_temp
-            self.c_current = c_temp
+        if self.accept.accept(self.cost_current, c_temp, self.sol_current, x_temp, self.stats["iterations"]):
+            self.sol_current = x_temp
+            self.cost_current = c_temp
 
-        self.iteration_number += 1
+            #pair = (self.stats["iterations"], self.cost_current.__round__(2))
+            #self.stats["current_cost_per_iteration"].append(pair)
+
+        # Update stats
+        self.stats["average_visited_length"] += x_temp.number_of_tokens()
+        self.stats["iterations"] += 1
+        self.stats["time_destroy"] += t_d - t_b
+        self.stats["time_repair"] += t_r - t_d
+        self.stats["time_cost"] += t_c - t_r
 
         # Repeat if no fully correct solution found
         return c_temp != 0
 
     def extend_result(self, res: SearchResult) -> SearchResult:
-        res.dictionary["best_cost_per_iteration"] = self.best_cost_per_iteration
-        res.dictionary["current_cost_per_iteration"] = self.current_cost_per_iteration
-        res.dictionary["time_destroy"] = self.time_destroy
-        res.dictionary["time_repair"] = self.time_repair
-        res.dictionary["time_cost"] = self.time_cost
-        res.dictionary["iterations"] = self.iteration_number - 1
+        # Round times for readability
+        self.stats["average_visited_length"] /= self.stats["iterations"]
+        self.stats["visited_programs"] = len(self.cost_dict)
+        self.stats["time_destroy"] = self.stats["time_destroy"].__round__(3)
+        self.stats["time_repair"] = self.stats["time_repair"].__round__(3)
+        self.stats["time_cost"] = self.stats["time_cost"].__round__(3)
+
+        self.stats.update(res.dictionary)
+        res.dictionary = self.stats
 
         return res
 
-    @staticmethod
-    def cost_seq(tc: list[Example], seq: SeqToken):
-        def ex_cost(ex: Example):
-            try:
-                inp = copy.deepcopy(ex.input_environment)
-                return seq.apply(inp).distance(ex.output_environment)
-            except (InvalidTransition, LoopIterationLimitReached):
-                return float('inf')
+    def debug_print(self, msg: str):
+        if self.debug:
+            print(msg)
 
-        return mean([ex_cost(ex) for ex in tc])
+    cost_dict = {}
+
+    def eff_cost(self, test_case: list[Example], program: Program):
+        key = str(program)
+
+        if key not in self.cost_dict:
+            self.cost_dict[key] = self.cost(test_case, program)
+            self.stats["explored_per_depth"][self.stats["search_depth"]] += 1
+        else:
+            self.stats["multiple_explored_per_depth"][self.stats["search_depth"]] += 1
+
+        return self.cost_dict[key]
