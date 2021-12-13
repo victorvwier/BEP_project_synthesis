2672eacbd0e200e1e5a32401c14050119d0228e2
Stef <rasing.stef@gmail.com>
2021-12-13 10:25:09 +0100

    implemented new destroy methods

--- search/vlns/large_neighborhood_search/destroy/destroy.py
+++ search/vlns/large_neighborhood_search/destroy/destroy.py
@@ -1,27 +1,16 @@
-from common.prorgam import Program
-from search.vlns.large_neighborhood_search.tokens.destroyed_token import DestroyedToken
-from search.vlns.large_neighborhood_search.tokens.sequence_token import SequenceToken, SeqToken, EmptySequenceToken
+from common.prorgam import Program, EnvToken
 
 
 class Destroy:
-    """Interface for a Destroy method."""
+    """Interface for destroy methods."""
 
-    def destroy(self, solution: Program) -> SeqToken:
-        """Destructs a given 'solution'. Returns a SequenceToken containing, among others, DestroyedTokens."""
-
-        if len(solution.sequence) < 3:
-            return SequenceToken(
-                DestroyedToken(destroyed_token=None),
-                SequenceToken(
-                    DestroyedToken(destroyed_token=None),
-                    EmptySequenceToken()))
-
-        seq = SequenceToken.from_list(solution.sequence)
-
-
-        return self.destroy_sequence(seq)
-
-    def destroy_sequence(self, seq: SeqToken) -> SeqToken:
-        """Destroys a given SequenceToken. Should return the destroyed SequenceToken"""
+    def destroy(self, program: Program) -> list[list[EnvToken]]:
+        """Destructs a given program. Returns a list of token sequences."""
 
         raise NotImplementedError()
+
+    def set_search_depth(self, n: int):
+        pass
+
+    def increment_search_depth(self):
+        pass
