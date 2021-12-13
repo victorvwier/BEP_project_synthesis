318f72c0e677849b6f3a0afc275214636a659f46
Stef <rasing.stef@gmail.com>
2021-12-13 11:54:09 +0100

    bug fixes

--- tests/common_environment/test_control_tokens.py
+++ tests/common_environment/test_control_tokens.py
@@ -7,8 +7,8 @@ from common.tokens.string_tokens import *
 
 class TestIf(unittest.TestCase):
     def test_simple(self):
-        e1 = StringEnvironment("Hello, World!")
-        e2 = StringEnvironment("hello, World!")
+        e1 = StringEnvironment(list("Hello, World!"))
+        e2 = StringEnvironment(list("hello, World!"))
         p = Program([
             If(IsUppercase(), [MakeLowercase()], [MakeUppercase()])
         ])
@@ -20,9 +20,9 @@ class TestIf(unittest.TestCase):
         self.assertEqual(e2.to_string(), "Hello, World!")
 
     def test_nested(self):
-        e1 = StringEnvironment("hello, World!")
-        e2 = StringEnvironment("Hello, World!")
-        e3 = StringEnvironment("#ello, World!")
+        e1 = StringEnvironment(list("hello, World!"))
+        e2 = StringEnvironment(list("Hello, World!"))
+        e3 = StringEnvironment(list("#ello, World!"))
         p = Program([
             If(IsUppercase(),
                [MakeLowercase()],
@@ -40,28 +40,9 @@ class TestIf(unittest.TestCase):
         self.assertEqual(e2.to_string(), "hello, World!")
         self.assertEqual(e3.to_string(), "ello, World!")
 
-class TestRecurse(unittest.TestCase):
-    def test_simple(self):
-        e1 = StringEnvironment("hello, world!")
-        p = Program([
-            LoopWhile(NotAtEnd(), [MakeUppercase(), MoveRight()]), MakeUppercase()
-        ])
-
-        e1 = p.interp(e1)
-        self.assertEqual(e1.to_string(), "HELLO, WORLD!")
-
-    def test_limit(self):
-        e1 = StringEnvironment("hello, world!")
-        p = Program([
-            LoopWhile(NotAtEnd(), [MakeUppercase(), ]), MakeUppercase()
-        ])
-
-        self.assertRaises(LoopIterationLimitReached, lambda : p.interp(e1))
-
-
 class TestLoop(unittest.TestCase):
     def test_simple(self):
-        e1 = StringEnvironment("hello, world!")
+        e1 = StringEnvironment(list("hello, world!"))
         p = Program([
             LoopWhile(NotAtEnd(), [MakeUppercase(), MoveRight()]),
             MakeUppercase()
@@ -71,7 +52,7 @@ class TestLoop(unittest.TestCase):
         self.assertEqual(e1.to_string(), "HELLO, WORLD!")
 
     def test_limit(self):
-        e1 = StringEnvironment("hello, world!")
+        e1 = StringEnvironment(list("hello, world!"))
         p = Program([
             LoopWhile(NotAtEnd(), [])
         ])
