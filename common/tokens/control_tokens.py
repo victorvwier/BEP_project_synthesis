fe78e1a514248bad817395fa4269ea717daf60f3
Stef <rasing.stef@gmail.com>
2021-12-13 11:24:34 +0100

    Removed not used recurse token

--- common/tokens/control_tokens.py
+++ common/tokens/control_tokens.py
@@ -45,54 +45,6 @@ class If(ControlToken):
             )
         return result
 
-class Recurse(ControlToken):
-    """Recursive calling ControlToken."""
-
-    def __init__(self, cond: Union[None, BoolToken], base_case: List[EnvToken], recursive_case: List[EnvToken]):
-        """Creates a new Recurse ControlToken. When applied, 'cond' is executed. If that yields true, 'recursive_case' is executed and the whole program is called recursively, otherwise 'base_case' is executed. Note that this Token needs a pointer to its parent program to be able to call it recursively."""
-        self.cond = cond
-        self.base_case = base_case
-        self.recursive_case = recursive_case
-        self.calls = 0
-
-    def apply(self, env: Environment) -> Environment:
-        # Raise exception if recursive call limit is reached
-        if self.calls >= env.program.recursive_call_limit:
-            raise RecursiveCallLimitReached()
-
-        self.calls += 1
-
-        # if the condition is None or true, make recursive call
-        if self.cond is None or self.cond.apply(env):
-            env = Program(self.recursive_case).interp(env, False)
-            return env.program.interp(env)
-
-        # else, base case
-        return Program(self.base_case).interp(env, False)
-
-    def number_of_tokens(self, control_cost=2) -> int:
-        return control_cost + \
-               sum([t.number_of_tokens(control_cost) for t in self.base_case]) + \
-               sum([t.number_of_tokens(control_cost) for t in self.recursive_case])
-
-    def __str__(self):
-        return "Recurse(%s [%s] [%s])" % \
-               (self.cond, ", ".join(list(map(str, self.base_case))), ", ".join(list(map(str, self.recursive_case))))
-
-    def __repr__(self):
-        return "Recurse(%s [%s] [%s])" % \
-               (self.cond, ", ".join(list(map(str, self.base_case))), ", ".join(list(map(str, self.recursive_case))))
-
-    def to_formatted_string(self):
-        result = "while %s do:\n\t%s" % (
-            self.cond.to_formatted_string(),
-            "\n\t".join([t.to_formatted_string().replace("\n", "\n\t") for t in self.recursive_case])
-        )
-        if self.base_case:
-            result += "\nfinally:\n\t%s" % (
-                "\n\t".join([t.to_formatted_string().replace("\n", "\n\t") for t in self.base_case])
-            )
-        return result
 
 class LoopWhile(ControlToken):
     """Loop ControlToken."""
@@ -137,11 +89,6 @@ class LoopWhile(ControlToken):
             "\n\t".join([t.to_formatted_string().replace("\n", "\n\t") for t in self.loop_body])
         )
         return result
-
-
-class RecursiveCallLimitReached(Exception):
-    """"Exception raised when the recursive call limit, set in the Program constructor is reached."""
-    pass
 
 
 class LoopIterationLimitReached(Exception):
