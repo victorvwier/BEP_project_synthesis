ab5f2c8fd77d9806ede9d97dc39e341c29743800
Victor van WIeringen <Victorvwier@hotmail.com>
2021-12-13 14:14:58 +0100

    Fix

--- main.py
+++ main.py
@@ -11,26 +11,6 @@ from search.batch_run import BatchRun
 from search.vlns.large_neighborhood_search.algorithms.remove_n_insert_n import RemoveNInsertN
 
 if __name__ == "__main__":
-    result = BatchRun(
-        # Task domain
-        domain="robot",
-
-        # Iterables for files name. Use [] to use all values.
-        # This runs all files adhering to format "2-*-[0 -> 10]"
-        # Thus, ([], [], []) runs all files for a domain.
-        files=([], [], []),
-
-        # Search algorithm to be used
-        search_algorithm=AStar(10),
-
-        # Prints out result when a test case is finished
-        print_results=True,
-
-        # Use multi core processing
-        multi_core=True,
-    ).run()
-
-    """
     searchAlgos : List[Type[SearchAlgorithm]] = [
         [MetropolisHasting, "metro"],
         [Brute, "brute"],
@@ -40,14 +20,27 @@ if __name__ == "__main__":
         [AStar, "Astar"]
     ]
 
+
     results = []
     for alg in searchAlgos:
-        experiment: Experiment = RobotParser().parse_specific_range(
-            range(1, 3), range(0, 10), range(0, 10))
-        (ave_suc, ave_time, com_suc) = test_performance_single_experiment(
-            experiment, alg[0])
-        results.append((alg[1], com_suc))
+        result = BatchRun(
+        # Task domain
+        domain="robot",
+
+        # Iterables for files name. Use [] to use all values.
+        # This runs all files adhering to format "2-*-[0 -> 10]"
+        # Thus, ([], [], []) runs all files for a domain.
+        files=([], [], []),
+
+        # Search algorithm to be used
+        search_algorithm=alg[0](10),
+
+        # Prints out result when a test case is finished
+        print_results=True,
+
+        # Use multi core processing
+        multi_core=True,
+    ).run()
 
     for res in results:
         print(res[0], res[1])
-    """
