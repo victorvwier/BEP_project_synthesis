ecfbd7dacb9b042ee12be09a5ea1bfe6cfef3f52
Stef <rasing.stef@gmail.com>
2021-12-21 14:52:56 +0100

    run

--- main.py
+++ main.py
@@ -9,42 +9,46 @@ from search.metropolis_hastings.metropolis import MetropolisHasting
 from search.vlns.large_neighborhood_search.algorithms.remove_n_insert_n import RemoveNInsertN
 from search.batch_run import BatchRun
 from search.vlns.large_neighborhood_search.algorithms.remove_n_insert_n import RemoveNInsertN
+from search.vlns.large_neighborhood_search.algorithms.remove_n_insert_n_vdi import RemoveNInsertNVDI
 
 if __name__ == "__main__":
     searchAlgos : List[Type[SearchAlgorithm]] = [
-        [MetropolisHasting, "metro"],
+        #[MetropolisHasting, "metro"],
         [Brute, "brute"],
-        [MCTS, "mcts"],
-        [VanillaGP, "gp"],
+        #[MCTS, "mcts"],
+        #[VanillaGP, "gp"],
         [RemoveNInsertN, "VLNS"],
-        [AStar, "Astar"]
+        [RemoveNInsertNVDI, "VLNS_vdi"],
+        #[AStar, "Astar"]
     ]
 
+    domains : list[str] = ["robot"]
 
     results = []
     for alg in searchAlgos:
-        result = BatchRun(
-        # Task domain
-        domain="robot",
+        for domain in domains:
+            result = BatchRun(
+            # Task domain
+            domain=domain,
 
-        # Iterables for files name. Use [] to use all values.
-        # This runs all files adhering to format "2-*-[0 -> 10]"
-        # Thus, ([], [], []) runs all files for a domain.
-        files=([], [], []),
+            # Iterables for files name. Use [] to use all values.
+            # This runs all files adhering to format "2-*-[0 -> 10]"
+            # Thus, ([], [], []) runs all files for a domain.
+            files=([], [], []),
 
-        # Search algorithm to be used
-        search_algorithm=alg[0](10),
+            # Search algorithm to be used
+            search_algorithm=alg[0](10),
 
-        # Prints out result when a test case is finished
-        print_results=True,
+            # Prints out result when a test case is finished
+            print_results=True,
 
-        # Use multi core processing
-        multi_core=True,
+            # Use multi core processing
+            multi_core=True,
 
-        # Use file_name= to append to a file whenever a run got terminated
-        # Comment out argument to create new file.
-        #file_name="VLNS-20211213-162128.txt"
-    ).run()
+            # Use file_name= to append to a file whenever a run got terminated
+            # Comment out argument to create new file.
+            #file_name="VLNS-20211213-162128.txt"
+        ).run()
 
-    for res in results:
-        print(res[0], res[1])
+    # for res in results:
+    #     print(res[0], res[1])
