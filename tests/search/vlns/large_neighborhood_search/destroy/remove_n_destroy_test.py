97ad8b8154793be85c3bea297a2f05a539c38359
Stef <rasing.stef@gmail.com>
2021-12-21 10:30:48 +0100

    refactor

--- tests/search/vlns/large_neighborhood_search/destroy/remove_n_destroy_test.py
+++ tests/search/vlns/large_neighborhood_search/destroy/remove_n_destroy_test.py
@@ -2,7 +2,7 @@ import unittest
 
 from common.prorgam import Program
 from common.tokens.string_tokens import *
-from search.vlns.large_neighborhood_search.destroy.remove_n_destroy import ExtractNDestroy
+from search.vlns.large_neighborhood_search.destroy.remove_n_destroy_old import ExtractNDestroy
 
 
 class MyTestCase(unittest.TestCase):
