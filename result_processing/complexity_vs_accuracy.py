2cd710d0f684931a92bd4b1153193bce51458e6e
Stef <rasing.stef@gmail.com>
2022-05-02 14:40:19 +0200

    Added results

--- result_processing/complexity_vs_accuracy.py
+++ result_processing/complexity_vs_accuracy.py
@@ -1,34 +1,31 @@
 from result_processing.result_parser import Results
 
-domain = "R"
-setting = "G"
-graph = ["compl_vs_acc", "compl_vs_exe"][0]
 
-colors = {
-    "Brute": "xkcd:grey",
-    "AS": "xkcd:chocolate",
-    "GP": "xkcd:tomato",
-    "LNS": "xkcd:orange",
-    "MCTS": "xkcd:gold",
-    "MH": "yellowgreen",
-}
+def run(domain, setting, graph):
+    colors = {
+        "Brute": "xkcd:grey",
+        "AS": "xkcd:chocolate",
+        "GP": "xkcd:tomato",
+        "LNS": "xkcd:orange",
+        "MCTS": "xkcd:gold",
+        "MH": "yellowgreen",
+    }
 
-algorithms = ["Brute", "AS", "GP", "LNS", "MCTS", "MH"]
-#algorithms = ["Brute", "AS"]
-data = []
-for a in algorithms:
-    data.append({"name": a, "file": "../results/{}/{}-{}{}-PC.txt".format(a, a, domain, setting)})
+    algorithms = ["Brute", "AS", "GP", "LNS", "MCTS", "MH"]
+    #algorithms = ["Brute", "AS"]
+    data = []
+    for a in algorithms:
+        data.append({"name": a, "file": "../results/{}/{}-{}{}-PC.txt".format(a, a, domain, setting)})
 
-complexity = {
-    "R": "Grid size",
-    "S": "#examples",
-    "P": "Matrix size",
-}[domain]
+    complexity = {
+        "R": "Grid size",
+        "S": "#examples",
+        "P": "Matrix size",
+    }[domain]
 
 
 
 # Complexity vs accuracy
-if __name__ == "__main__":
     if graph == "compl_vs_acc":
         results = Results(data, colors)
         results.filter_fields(["complexity", "correct"])
@@ -56,3 +53,19 @@ if __name__ == "__main__":
             y_axis="Average execution time (sec)",
         )
         results.save(1, domain, "complexity_vs_execution_time_correct_{}-{}".format(domain, setting))
+
+if __name__ == "__main__":
+
+    run("P", "G", "compl_vs_acc")
+    run("P", "G", "compl_vs_exe")
+
+
+    """
+    for domain in ["R", "P", "S"]:
+        for setting in ["E", "G", "O"]:
+            for graph in ["compl_vs_acc", "compl_vs_exe"]:
+                try:
+                    run(domain, setting, graph)
+                except FileNotFoundError:
+                    print(domain, setting)
+    """
